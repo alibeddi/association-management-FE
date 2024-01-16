e48aa95f85d5dd60facbd68283156c44519f92d0
SirineSarray <sirine2019sarray@gmail.com>
2024-01-16 21:46:59 +0100

    add the fetch permissions function

--- src/redux/slices/permissions.ts
+++ src/redux/slices/permissions.ts
@@ -1,6 +1,7 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { Permission } from 'src/@types/Permission';
 import { IStatus } from 'src/@types/status';
+import axios from '../../utils/axios';
 
 type PermissionState = {
   permissions: Permission[];
@@ -11,6 +12,20 @@ const initialState: PermissionState = {
   permissions: [] as Permission[],
   status: IStatus.IDLE,
 };
+
+export const getPermissions = createAsyncThunk('permissions/GETALL', async () => {
+  let data;
+  try {
+    const response = await axios.get(`/permissions`);
+    data = await response.data;
+    if (response.status === 200) {
+      return data.data;
+    }
+    throw new Error(response.statusText);
+  } catch (err) {
+    return Promise.reject(err.message ? err.message : data?.message);
+  }
+});
 
 const slice = createSlice({
   name: 'permissions',
