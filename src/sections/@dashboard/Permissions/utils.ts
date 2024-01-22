5724191fc360efbe4dbeb1ff231f716ed8ded381
sirine.sarray <sirinesarray@takiacademyteam.com>
2024-01-22 11:35:57 +0100

    fi the group permissions guard

--- src/sections/@dashboard/Permissions/utils.ts
+++ src/sections/@dashboard/Permissions/utils.ts
@@ -1,7 +1,7 @@
 import { Permission } from 'src/@types/Permission';
 
 export function hasPermission(userPermissions: Permission[], model: string, method: string) {
-  return userPermissions.some(
+  return userPermissions?.some(
     (permission) => permission.model === model && permission.method === method
   );
 }
