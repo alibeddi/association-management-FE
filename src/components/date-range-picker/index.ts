03026516452387951230ee7079f08d19302544ba
moataz hjaiji <moatazhjaiji@takiacademyteam.com>
2024-02-06 14:32:25 +0100

    add model login form in login page to login with takiacademy

--- src/components/date-range-picker/index.ts
+++ src/components/date-range-picker/index.ts
@@ -2,4 +2,4 @@ export * from './types';
 
 export { default as useDateRangePicker } from './useDateRangePicker';
 
-export { default } from './DateRangePicker';
+export { default as DateRangePicker} from './DateRangePicker';
