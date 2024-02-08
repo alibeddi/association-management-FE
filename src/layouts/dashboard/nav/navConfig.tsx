c29227b3c426d1c1549c7a5b2ab1b284a3fc6b14
sirine.sarray <sirinesarray@takiacademyteam.com>
2024-02-08 09:26:43 +0100

    move the icons to another path

--- src/layouts/dashboard/nav/navConfig.tsx
+++ src/layouts/dashboard/nav/navConfig.tsx
@@ -3,29 +3,21 @@ import { AuthUserType } from '../../../auth/types';
 import SvgColor from '../../../components/svg-color';
 import { PATH_DASHBOARD } from '../../../routes/paths';
 import { hasPermission } from '../../../sections/@dashboard/Permissions/utils';
+import { ic_operators, ic_settings, ic_lock, ic_calendar } from '../../../assets/icons/navbar';
 
-const icon = (name: string) => (
-  <SvgColor src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />
-);
+const icon = (iconSrc: string) => <SvgColor src={iconSrc} sx={{ width: 1, height: 1 }} />;
 
 const ICONS = {
-  operators: icon('ic_operators'),
-  fleet: icon('ic_vehicle'),
-  analytics: icon('ic_analytics'),
-  dashboard: icon('ic_dashboard'),
-  branches: icon('ic_branches'),
-  reservations: icon('ic_reservations'),
-  pricing: icon('ic_pricing'),
-  settings: icon('ic_settings'),
-  groupPermissions: icon('ic_lock'),
-  calendar: icon('ic_calendar'),
+  operators: icon(ic_operators),
+  settings: icon(ic_settings),
+  groupPermissions: icon(ic_lock),
+  calendar: icon(ic_calendar),
 };
 
 export default function navConfigItems(user: AuthUserType) {
   const userPermissions = user?.permissionGroup[0].permissions;
   const hasAccessToKpis = hasPermission(userPermissions, ModelCode.KPI, MethodCode.LIST);
   const hasAccessToUsers = hasPermission(userPermissions, ModelCode.USER, MethodCode.LIST);
-  const hasAccessToCalendar = hasPermission(userPermissions, ModelCode.WORKTIME, MethodCode.LIST);
   const hasAccessToGroupPermissions = hasPermission(
     userPermissions,
     ModelCode.PERMISSION_GROUP,
@@ -58,7 +50,7 @@ export default function navConfigItems(user: AuthUserType) {
           title: 'calendar',
           path: PATH_DASHBOARD.calender,
           icon: ICONS.calendar,
-          tobeDisplayed: hasAccessToCalendar,
+          tobeDisplayed: true,
         },
       ],
     },
