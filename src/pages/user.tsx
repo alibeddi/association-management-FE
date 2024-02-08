901c5ec026acd20d089b5e5cf89ffa9acc2af126
fares.talbi <farestalbi@takiacademyteam.com>
2024-02-08 12:07:22 +0100

    fix user ts

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -3,7 +3,6 @@ import { Helmet } from 'react-helmet-async';
 import { useSettingsContext } from '../components/settings';
 import UserListPage from '../sections/@dashboard/user';
 
-
 export default function UserList() {
   const { themeStretch } = useSettingsContext();
   return (
