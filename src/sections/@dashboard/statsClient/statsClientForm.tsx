be2296e701d2ac85746fa10d753eeffaea046985
moataz hjaiji <moatazhjaiji@takiacademyteam.com>
2024-02-14 10:43:27 +0100

    add select

--- src/sections/@dashboard/statsClient/statsClientForm.tsx
+++ src/sections/@dashboard/statsClient/statsClientForm.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@mui/material';
-import { Box } from '@mui/system';
+import { Box, Stack } from '@mui/system';
 import React, { useEffect, useRef, useState } from 'react';
 import {  useFieldArray, useForm } from 'react-hook-form';
 import FormProvider, { RHFAutocomplete, RHFSelect } from '../../../components/hook-form'
@@ -9,24 +9,20 @@ import { RootState, useDispatch, useSelector } from '../../../redux/store';
 
 
 const StatsClientForm = () => {
-  
-  const methods = useForm();
-  const {control,handleSubmit,setValue,register} = methods
+  const [select,setSelect] = useState<{num:number,value:string}[] | []>([]);
   const dispatch = useDispatch()
-  const [numSelect,setNumSelect] = useState<number>(0)
-  const [select,setSelect] = useState<number[] | []>([]);
-  const  { fields, append, prepend, remove, swap, move, insert } = useFieldArray({
-    control, // control props comes from useForm (optional: if you are using FormContext)
-    name: "test", // unique name for your Field Array
-  });
+  const [numSelect,setNumSelect] = useState(0)
   const addSelect = () => {
-    setNumSelect(prevNum => prevNum + 1);
+    setNumSelect(pre => pre +  1);
+    console.log({numSelect})
+    setSelect(pre=> [...pre,{value:"",num:numSelect}])
   };
-
-  const removeSelect = (index: number) => {
-    setValue(`stats-client-${index}`, '');
-    setNumSelect(prevNum => prevNum - 1);
-  };
+  const removeSelect = (index:number) => setSelect((prevState) => {
+    console.log("index ", index)
+    const newArray = [...prevState];
+    newArray.splice(index,  1);
+    return newArray;
+  });
 
   // TODO: make scroll to get 10 other
   useEffect(()=>{
@@ -34,97 +30,59 @@ const StatsClientForm = () => {
       page:0,limit:10
      }))
   },[dispatch])
+  const methods = useForm();
 
   const {kpis} = useSelector((state:RootState)=>state.kpis)
-  
+  const {control,handleSubmit,unregister} = methods
   const submit = (data:any) => console.log({data})
 
   return (
-    <FormProvider methods={methods} onSubmit={handleSubmit(submit)} >
- {/* {select.length > 0 && select.map((_, index) => (
-          <>            
-          <Box
-          key={index}
-            >
-                <RHFAutocomplete
-                  key={index}
-                  name={`stats-client-${index}`}
-                  freeSolo
-                  getOptionLabel={(option) => (typeof option === 'string' ? option : option.name || '')}
-                  options={kpis.docs}
-                />
-            </Box>
-            <Button
-            variant='contained'
-            color="error"
-            onClick={()=>removeSelect(index)}
-            >
-              Remove item
-            </Button>
-          </>
+    <FormProvider methods={methods} onSubmit={handleSubmit(submit)} style={
+      {
+        display:'flex',
+        flexDirection:'column',
+        gap:'1rem'
+      }
+    }>
 
-            
-        ))} */}
-        {/* {numSelect ? [...Array(numSelect)].map((_, index) => (
-          <>            
-          <Box
-          key={index}
-            >
-                <RHFAutocomplete
-                  key={index}
-                  name={`stats-client-${index}`}
-                  freeSolo
-                  getOptionLabel={(option) => (typeof option === 'string' ? option : option.name || '')}
-                  options={kpis.docs}
-                />
-            </Box>
-            <Button
-            variant='contained'
-            color="error"
-            onClick={()=>removeSelect(index)}
-            >
-              Remove item
-            </Button>
-          </>
 
-            
-        )) : <div>nothing</div>} */}
- {/* {fields.map((field, index) => (
-        <RHFAutocomplete
-          key={field.id} // Ensure each component has a unique key
-          name={`selects[${index}].value`} // Set the field name dynamically
-          defaultValue={''} // Set default value if needed
-          freeSolo
-          getOptionLabel={(option) => (typeof option === 'string' ? option :  '')}
-          options={kpis}
-        />
-      ))} */}
-{
-  fields.map((field,index)=>(
-    <>            
-    <Box
-    key={index}
+ {
+  select.length >   0 && select.map((s,index)=>(   <>            
+    <Stack
+    key={s.num}
+    sx={{
+      flexDirection:'row',
+      display:'flex',
+      gap:'1rem',
+      width:"100%"
+    }}
       >
           <RHFAutocomplete
-            key={index}
+            name={`stats-client-${s.num}`}
             freeSolo
-            value={field.value}
-            {...register(`test.${index}.value`)}
             getOptionLabel={(option) => (typeof option === 'string' ? option : option.name || '')}
             options={kpis.docs}
+            sx={{
+              flexBasis:'80%'
+            }}
           />
-      </Box>
-      <Button
+          <Button
       variant='contained'
       color="error"
-      onClick={()=>removeSelect(index)}
+      onClick={ ()=> {removeSelect(index);unregister(`stats-client-${s.num}`)}}
       >
         Remove item
       </Button>
-    </>
-  ))
-}
-     <Box>
+      </Stack>
+      
+    </>)) 
+ }
+     <Box
+     sx={{
+      display:'flex',
+      gap:'1rem'
+     }}
+     >
       <Button
       variant='contained'
         type='submit'
@@ -132,10 +90,8 @@ const StatsClientForm = () => {
 submit
       </Button>
      <Button
-          variant="contained"
-          onClick={()=> append({
-            test: ''
-          })}
+          variant="outlined"
+          onClick={()=> addSelect()}
           >
             add another task
           </Button>
