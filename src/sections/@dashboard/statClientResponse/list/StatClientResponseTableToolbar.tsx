322146f79be11e917e59cebd567aec950569c1de
moataz hjaiji <moatazhjaiji@takiacademyteam.com>
2024-03-01 15:55:29 +0100

    remove uncessary comment

--- src/sections/@dashboard/statClientResponse/list/StatClientResponseTableToolbar.tsx
+++ src/sections/@dashboard/statClientResponse/list/StatClientResponseTableToolbar.tsx
@@ -47,30 +47,7 @@ export default function StatClientResponseTableToolbar({
       }}
       sx={{ px: 2.5, py: 3 }}
     >
-      {/* <TextField
-        fullWidth
-        value={filterClientName}
-        onChange={onFilterName}
-        placeholder={placeholder}
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <Iconify icon="eva:search-fill" sx={{ color: 'text.disabled' }} />
-            </InputAdornment>
-          ),
-        }}
-      /> */}
-      
-      {/* {isFiltered && (
-        <Button
-          color="error"
-          sx={{ flexShrink: 0 }}
-          onClick={onResetFilter}
-          startIcon={<Iconify icon="eva:trash-2-outline" />}
-        >
-          Clear
-        </Button>
-      )} */}
+     
       {filters.length > 0 && (
         <Button
           color="error"
