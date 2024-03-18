73a797dd18d7152be063079284a94bca19776976
sirine.sarray <sirinesarray@takiacademyteam.com>
2024-03-18 11:32:00 +0100

    disable the submit button when there is no filter

--- src/sections/@dashboard/statClientResponse/form/StatClientResponseFormFilter.tsx
+++ src/sections/@dashboard/statClientResponse/form/StatClientResponseFormFilter.tsx
@@ -2,7 +2,7 @@ import { LoadingButton } from '@mui/lab';
 import { Button } from '@mui/material';
 import { Stack } from '@mui/system';
 import { useSnackbar } from 'notistack';
-import  { useState } from 'react'
+import { useState } from 'react';
 import EmptyContent from '../../../../components/empty-content';
 import Iconify from '../../../../components/iconify';
 import { useTable } from '../../../../components/table';
@@ -13,77 +13,94 @@ import { validNotEmptyFilters } from '../../../../utils';
 import StatResponseFilterSelect from './StatResponseFilterSelect';
 
 type IProps = {
-  onClose : () => void;
-}
-const StatClientResponseFormFilter = ({
-  onClose
-}:IProps) =>  {
-  const {page,rowsPerPage} = useTable({defaultOrderBy: 'createdAt', defaultOrder: 'desc'})
-  const {enqueueSnackbar} = useSnackbar()
-  const [isSubmitting,setIsSubmitting] = useState<boolean>(false)
-  const {filters} = useSelector(store=>store.statClientResponses)
+  onClose: () => void;
+};
+const StatClientResponseFormFilter = ({ onClose }: IProps) => {
+  const { page, rowsPerPage } = useTable({ defaultOrderBy: 'createdAt', defaultOrder: 'desc' });
+  const { enqueueSnackbar } = useSnackbar();
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+  const { filters } = useSelector((store) => store.statClientResponses);
   const handleSubmit = async () => {
-    setIsSubmitting(true)
+    setIsSubmitting(true);
     const isValid = validNotEmptyFilters(filters);
-    if(!isValid){ setIsSubmitting(false)
-      enqueueSnackbar("Please ensure all fields are filled in.",{
-        variant:"error"
-      })
+    if (!isValid) {
+      setIsSubmitting(false);
+      enqueueSnackbar('Please ensure all fields are filled in.', {
+        variant: 'error',
+      });
       return;
     }
-    await dispatch(statsClientResponseFilter({
-      page: page+1,
-      limit:rowsPerPage,
-      filterValue:filters
-    })).unwrap().then(res=>{enqueueSnackbar("success");onClose();}).catch(err=>enqueueSnackbar(err.message,{
-      variant:"error"
-    }))
-    setIsSubmitting(false)
-  }
+    await dispatch(
+      statsClientResponseFilter({
+        page: page + 1,
+        limit: rowsPerPage,
+        filterValue: filters,
+      })
+    )
+      .unwrap()
+      .then((res) => {
+        enqueueSnackbar('success');
+        onClose();
+      })
+      .catch((err) =>
+        enqueueSnackbar(err.message, {
+          variant: 'error',
+        })
+      );
+    setIsSubmitting(false);
+  };
 
-  
   return (
     <Stack>
+      <Button onClick={() => dispatch(addFilter())} startIcon={<Iconify icon="icons8:plus" />}>
+        {' '}
+        Add new Filter
+      </Button>
 
- <Button onClick={()=>dispatch(addFilter())} startIcon={<Iconify icon='icons8:plus' />}> Add new Filter</Button>
+      <Stack
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '1rem',
+          minHeight: '35rem',
+          minWidth: '35rem',
+          justifyContent: 'space-between',
+          padding: '1rem',
+          flex: 1,
+        }}
+      >
+        {filters?.length > 0 ? (
+          <StatResponseFilterSelect filters={filters} />
+        ) : (
+          <EmptyContent title="no filter" />
+        )}
 
-   
-    <Stack  style={{
-      display: 'flex',
-      flexDirection: 'column',
-      gap: '1rem',
-      minHeight: '35rem',
-      minWidth:"35rem",
-      justifyContent: 'space-between',
-      padding:"1rem",
-      flex:1
-    }}>
-      {
-        filters?.length > 0 ? <StatResponseFilterSelect filters={filters}  /> : <EmptyContent title="no filter"/>
-      }
+        <Stack
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            '& *': {
+              flexBasis: '100%',
+            },
 
-      <Stack sx={{
-        display:"flex",
-        flexDirection:"row",
-        "& *": {
-          flexBasis: "100%",
-          
-        },
-        
-        gap:'1rem',
-
-      }}>
-      <LoadingButton variant="contained" type="submit" loading={isSubmitting} onClick={()=>handleSubmit()}>
-          submit
-        </LoadingButton>
-        <LoadingButton onClick={()=>onClose()} variant="outlined" loading={isSubmitting}>
-          cancel
-        </LoadingButton>
+            gap: '1rem',
+          }}
+        >
+          <LoadingButton
+            disabled={filters.length === 0}
+            variant="contained"
+            type="submit"
+            loading={isSubmitting}
+            onClick={() => handleSubmit()}
+          >
+            submit
+          </LoadingButton>
+          <LoadingButton onClick={() => onClose()} variant="outlined" loading={isSubmitting}>
+            cancel
+          </LoadingButton>
+        </Stack>
       </Stack>
     </Stack>
-    </Stack>
-
-  )
-
-}
-export default StatClientResponseFormFilter
+  );
+};
+export default StatClientResponseFormFilter;
